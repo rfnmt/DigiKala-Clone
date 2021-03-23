23864cc203627f8edb1dad61287f9ee6627099c7
Erfan <erfann022@gmail.com>
2021-03-23 13:46:43 +0430

    v0.21 Link to category in dropdown and cursor pointer for the category items added

--- src/Components/CategoryDropDown/index.jsx
+++ src/Components/CategoryDropDown/index.jsx
@@ -21,117 +21,121 @@ const CategoryDropDown = (props) => {
 
   return (
     <div className={Style.catContent}>
-      <div
-        className={
-          active === true ? Style.catListItemActive : Style.catListItem
-        }
-      >
-        <Link href="#">
-          <a>کالای دیجیتال</a>
-        </Link>
-        <div
-          className={Style.subCat}
-          style={{ display: `${active === true ? "block" : "none"}` }}
-        >
-          <Digital />
-        </div>
-      </div>
-      <div
-        className={Style.catListItem}
-        onMouseOver={activate}
-        onMouseLeave={deactivate}
-      >
-        <Link href="#">
-          <a>خودرو</a>
-        </Link>
-        <div className={Style.subCat}>
-          <Car />
-        </div>
-      </div>
-      <div
-        className={Style.catListItem}
-        onMouseOver={activate}
-        onMouseLeave={deactivate}
-      >
-        <Link href="#">
-          <a>مد و پوشاک</a>
-        </Link>
-        <div className={Style.subCat}>
-          <Digital />
-        </div>
-      </div>
-      <div
-        className={Style.catListItem}
-        onMouseOver={activate}
-        onMouseLeave={deactivate}
-      >
-        <Link href="#">
-          <a>اسباب بازی، کودک و نوزاد</a>
-        </Link>
-        <div className={Style.subCat}>
-          <Car />
-        </div>
-      </div>
-      <div
-        className={Style.catListItem}
-        onMouseOver={activate}
-        onMouseLeave={deactivate}
-      >
-        <Link href="#">
-          <a>خوردنی و آشامیدنی</a>
-        </Link>
-        <div className={Style.subCat}>
-          <Digital />
-        </div>
-      </div>
-      <div
-        className={Style.catListItem}
-        onMouseOver={activate}
-        onMouseLeave={deactivate}
-      >
-        <Link href="#">
-          <a>زیبایی و سلامت</a>
-        </Link>
-        <div className={Style.subCat}>
-          <Car />
-        </div>
-      </div>
-      <div
-        className={Style.catListItem}
-        onMouseOver={activate}
-        onMouseLeave={deactivate}
-      >
-        <Link href="#">
-          <a>خانه و آشپزخانه</a>
-        </Link>
-        <div className={Style.subCat}>
-          <Digital />
-        </div>
-      </div>
-      <div
-        className={Style.catListItem}
-        onMouseOver={activate}
-        onMouseLeave={deactivate}
-      >
-        <Link href="#">
-          <a>کتاب، لوازم التحریر و هنر</a>
-        </Link>
-        <div className={Style.subCat}>
-          <Car />
-        </div>
-      </div>
-      <div
-        className={Style.catListItem}
-        onMouseOver={activate}
-        onMouseLeave={deactivate}
-      >
-        <Link href="#">
-          <a>ورزش و سفر</a>
-        </Link>
-        <div className={Style.subCat}>
-          <Digital />
-        </div>
-      </div>
+      <Link href="http://localhost:3000/category">
+        <a>
+          <div
+            className={
+              active === true ? Style.catListItemActive : Style.catListItem
+            }
+          >
+            <Link href="#">
+              <a>کالای دیجیتال</a>
+            </Link>
+            <div
+              className={Style.subCat}
+              style={{ display: `${active === true ? "block" : "none"}` }}
+            >
+              <Digital />
+            </div>
+          </div>
+          <div
+            className={Style.catListItem}
+            onMouseOver={activate}
+            onMouseLeave={deactivate}
+          >
+            <Link href="#">
+              <a>خودرو</a>
+            </Link>
+            <div className={Style.subCat}>
+              <Car />
+            </div>
+          </div>
+          <div
+            className={Style.catListItem}
+            onMouseOver={activate}
+            onMouseLeave={deactivate}
+          >
+            <Link href="#">
+              <a>مد و پوشاک</a>
+            </Link>
+            <div className={Style.subCat}>
+              <Digital />
+            </div>
+          </div>
+          <div
+            className={Style.catListItem}
+            onMouseOver={activate}
+            onMouseLeave={deactivate}
+          >
+            <Link href="#">
+              <a>اسباب بازی، کودک و نوزاد</a>
+            </Link>
+            <div className={Style.subCat}>
+              <Car />
+            </div>
+          </div>
+          <div
+            className={Style.catListItem}
+            onMouseOver={activate}
+            onMouseLeave={deactivate}
+          >
+            <Link href="#">
+              <a>خوردنی و آشامیدنی</a>
+            </Link>
+            <div className={Style.subCat}>
+              <Digital />
+            </div>
+          </div>
+          <div
+            className={Style.catListItem}
+            onMouseOver={activate}
+            onMouseLeave={deactivate}
+          >
+            <Link href="#">
+              <a>زیبایی و سلامت</a>
+            </Link>
+            <div className={Style.subCat}>
+              <Car />
+            </div>
+          </div>
+          <div
+            className={Style.catListItem}
+            onMouseOver={activate}
+            onMouseLeave={deactivate}
+          >
+            <Link href="#">
+              <a>خانه و آشپزخانه</a>
+            </Link>
+            <div className={Style.subCat}>
+              <Digital />
+            </div>
+          </div>
+          <div
+            className={Style.catListItem}
+            onMouseOver={activate}
+            onMouseLeave={deactivate}
+          >
+            <Link href="#">
+              <a>کتاب، لوازم التحریر و هنر</a>
+            </Link>
+            <div className={Style.subCat}>
+              <Car />
+            </div>
+          </div>
+          <div
+            className={Style.catListItem}
+            onMouseOver={activate}
+            onMouseLeave={deactivate}
+          >
+            <Link href="#">
+              <a>ورزش و سفر</a>
+            </Link>
+            <div className={Style.subCat}>
+              <Digital />
+            </div>
+          </div>
+        </a>
+      </Link>
     </div>
   );
 };
